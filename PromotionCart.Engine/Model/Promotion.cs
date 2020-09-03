bb9d16a9d424381a44855812dee288be9b305355
rameshp4389 <70636040+rameshp4389@users.noreply.github.com>
2020-09-03 17:12:50 +0530

    adding promotion model

---
+++ PromotionCart.Engine/Model/Promotion.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace PromotionCart.Engine.Model
+{
+    public class Promotion
+    {
+        public int promotionID { get; set; }
+        public Dictionary<string, int> prodInfo { get; set; }
+        public decimal promotionPrice { get; set; }
+
+        public Promotion(int promoID, Dictionary<string, int> prodInfo, decimal pp)
+        {
+            this.promotionID = promoID;
+            this.prodInfo = prodInfo;
+            this.promotionPrice = pp;
+        }
+    }
+}
