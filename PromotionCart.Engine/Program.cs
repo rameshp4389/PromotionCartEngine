d340f1248cf535aeee8055d72338865137f9a0a9
rameshp4389 <70636040+rameshp4389@users.noreply.github.com>
2020-09-03 17:06:33 +0530

    adding initial files

---
+++ PromotionCart.Engine/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PromotionCart.Engine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Read();
+        }
+    }
+}
