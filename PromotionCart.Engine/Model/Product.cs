2d912a564089fb91bd42306cdbcc438612e03091
rameshp4389 <70636040+rameshp4389@users.noreply.github.com>
2020-09-03 17:09:31 +0530

    adding Product model

---
+++ PromotionCart.Engine/Model/Product.cs
@@ -0,0 +1,32 @@
+﻿namespace PromotionCart.Engine.Model
+{
+    public class Product
+    {
+        public string itemId { get; set; }
+        public decimal Price { get; set; }
+
+        public Product(string id)
+        {
+            this.itemId = id;
+            switch (id)
+            {
+                case "itemA":
+                    this.Price = 50m;
+
+                    break;
+                case "itemB":
+                    this.Price = 30m;
+
+                    break;
+                case "itemC":
+                    this.Price = 20m;
+
+                    break;
+                case "itemD":
+                    this.Price = 15m;
+                    break;
+            }
+        }
+    }
+}
+
