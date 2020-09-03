5f882b0bf0fb60d794474986e69cccc98a367513
rameshp4389 <70636040+rameshp4389@users.noreply.github.com>
2020-09-03 17:11:18 +0530

    adding order model

---
+++ PromotionCart.Engine/Model/Order.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace PromotionCart.Engine.Model
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public List<Product> Products { get; set; }
+
+        public Order(int _oid, List<Product> _prods)
+        {
+            this.OrderID = _oid;
+            this.Products = _prods;
+        }
+    }
+}
